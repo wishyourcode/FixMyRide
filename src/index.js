deaae967b4ee32225e18d36233184eab97aead6a
wishyourcode <vishal.m.mishra@slrtce.in>
2025-09-14 21:46:57 +0530

    Implement database connection and update environment configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`✅ Example app listening on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Failed to connect to the database", err);
+    process.exit(1);
+  });
